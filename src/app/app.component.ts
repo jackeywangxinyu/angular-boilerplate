4dc4e2439550fa84828a862ee1c12f865a28a14f
xinyu_wang <602507143@qq.com>
2023-12-08 18:36:58 +0900

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  profit = 15
   calculator = {
-    formula: '* 0.62 + 15',
+    formula: '* 0.062',
     inputValue: 0,
     outputValue: 0
   }
-  calculate(num: number, formula: string) {
+  calculate(num: number, formula: string, profit: number) {
+    console.log(profit);
+    
     const fullFormula = (num.toString() || 0) + formula;
     let result = 9999999;
     try {
-      result = eval(fullFormula);
+      result = eval(fullFormula) + Number(profit);
     } catch (error) {
       console.log(error);
       
     }
-    return result
+    return result.toFixed(2)
+  }
+  change(e: any) {
+    this.profit = e.value
   }
 }

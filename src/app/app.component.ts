6fa146d232a504603f74add329cf1fe0552a026c
xinyu_wang <602507143@qq.com>
2023-12-08 23:32:44 +0900

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,33 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  profit = 15
-  calculator = {
-    formula: '* 0.062',
-    inputValue: '',
-    outputValue: 0
-  }
-  calculate(numStr: string, formula: string, profit: number) {
-    const num = Number(numStr)
+  profit = '15';
+  inputValue = '';
+  formula = '0.062';
+  calculate(numStr: string, formula: string, profit: string) {
     let result = 9999999;
+
     try {
-      if (typeof num !== 'number') {
-        return ''
-      }
-      const fullFormula = (num || 0) + formula;
-      
-      result = eval(fullFormula) + Number(profit);
-      
+      const num = Number(numStr);
+      const formula_num = Number(formula);
+      const profit_num = Number(profit);
+
+      result = num * formula_num + profit_num;
     } catch (error) {
       console.log(error);
-      
     }
-    return result.toFixed(2)
-  }
-  change(e: any) {
-    this.profit = e.value
+    return result.toFixed(2);
   }
 }
